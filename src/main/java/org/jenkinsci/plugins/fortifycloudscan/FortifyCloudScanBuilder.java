e550fd7c4efcd6acce28ed3383fa17658391558e
stevespringett <steve@springett.us>
2015-10-20 12:35:55 -0500

    Added per job config items. Added execution and logging.

--- src/main/java/org/jenkinsci/plugins/fortifycloudscan/FortifyCloudScanBuilder.java
+++ src/main/java/org/jenkinsci/plugins/fortifycloudscan/FortifyCloudScanBuilder.java
@@ -36,6 +36,9 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * The fortifycloudscan builder class provides the ability to invoke a fortifycloudscan build as
@@ -51,18 +54,62 @@ public class FortifyCloudScanBuilder extends Builder implements Serializable {
     private static final long serialVersionUID = 5441945995905689815L;
 
     private final String buildId;
-    private final Boolean useSsc;
+    private final String xmx;
+    private final String buildLabel;
+    private final String buildProject;
+    private final String buildVersion;
+    private final boolean useSsc;
     private final String sscToken;
     private final String upToken;
+    private final String versionId;
+    private final String scanArgs;
+    private final String disableDefaultRuleType;
+    private final String encoding;
+    private final String filter;
+    private final boolean noDefaultIssueRules;
+    private final boolean noDefaultRules;
+    private final boolean noDefaultSourceRules;
+    private final boolean noDefaultSinkRules;
+    private final boolean disableSourceRendering;
+    private final boolean quick;
+    private final String rules;
+    private final String workers;
+    private final String rmiWorkerMaxHeap;
+    private final String threadCount;
 
 
     @DataBoundConstructor // Fields in config.jelly must match the parameter names
-    public FortifyCloudScanBuilder(String buildId, Boolean useSsc, String sscToken, String upToken) {
+    public FortifyCloudScanBuilder(String buildId, String xmx, String buildLabel, String buildProject,
+                                   String buildVersion, Boolean useSsc, String sscToken, String upToken,
+                                   String versionId, String scanArgs, String disableDefaultRuleType,
+                                   String encoding, String filter, Boolean noDefaultIssueRules,
+                                   Boolean noDefaultRules, Boolean noDefaultSourceRules,
+                                   Boolean noDefaultSinkRules, Boolean disableSourceRendering, Boolean quick,
+                                   String rules, String workers, String rmiWorkerMaxHeap, String threadCount) {
 
         this.buildId = buildId;
-        this.useSsc = useSsc;
+        this.xmx = xmx;
+        this.buildLabel = buildLabel;
+        this.buildProject = buildProject;
+        this.buildVersion = buildVersion;
+        this.useSsc = (useSsc != null) && useSsc;
         this.sscToken = sscToken;
         this.upToken = upToken;
+        this.versionId = versionId;
+        this.scanArgs = scanArgs;
+        this.disableDefaultRuleType = disableDefaultRuleType;
+        this.encoding = encoding;
+        this.filter = filter;
+        this.noDefaultIssueRules = (noDefaultIssueRules != null) && noDefaultIssueRules;
+        this.noDefaultRules = (noDefaultRules != null) && noDefaultRules;
+        this.noDefaultSourceRules = (noDefaultSourceRules != null) && noDefaultSourceRules;
+        this.noDefaultSinkRules = (noDefaultSinkRules != null) && noDefaultSinkRules;
+        this.disableSourceRendering = (disableSourceRendering != null) && disableSourceRendering;
+        this.quick = (quick != null) && quick;
+        this.rules = rules;
+        this.workers = workers;
+        this.rmiWorkerMaxHeap = rmiWorkerMaxHeap;
+        this.threadCount = threadCount;
     }
 
     /**
@@ -71,6 +118,38 @@ public class FortifyCloudScanBuilder extends Builder implements Serializable {
      */
     public String getBuildId() {
         return buildId;
+    }
+
+    /**
+     * Retrieves the XMX. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public String getXmx() {
+        return xmx;
+    }
+
+    /**
+     * Retrieves the build label. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public String getBuildLabel() {
+        return buildLabel;
+    }
+
+    /**
+     * Retrieves the build project. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public String getBuildProject() {
+        return buildProject;
+    }
+
+    /**
+     * Retrieves the build version. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public String getBuildVersion() {
+        return buildVersion;
     }
 
     /**
@@ -98,6 +177,126 @@ public class FortifyCloudScanBuilder extends Builder implements Serializable {
     }
 
     /**
+     * Retrieves the project version id. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public String getVersionId() {
+        return versionId;
+    }
+
+    /**
+     * Retrieves the scan arguments. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public String getScanArgs() {
+        return scanArgs;
+    }
+
+    /**
+     * Retrieves the disable default rule type. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public String getDisableDefaultRuleType() {
+        return disableDefaultRuleType;
+    }
+
+    /**
+     * Retrieves the encoding. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public String getEncoding() {
+        return encoding;
+    }
+
+    /**
+     * Retrieves the filter. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public String getFilter() {
+        return filter;
+    }
+
+    /**
+     * Retrieves value of no default issue rules. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public Boolean getNoDefaultIssueRules() {
+        return noDefaultIssueRules;
+    }
+
+    /**
+     * Retrieves value of no default rules. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public Boolean getNoDefaultRules() {
+        return noDefaultRules;
+    }
+
+    /**
+     * Retrieves value of no default source rules. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public Boolean getNoDefaultSourceRules() {
+        return noDefaultSourceRules;
+    }
+
+    /**
+     * Retrieves value of no default sink rules. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public Boolean getNoDefaultSinkRules() {
+        return noDefaultSinkRules;
+    }
+
+    /**
+     * Retrieves value of disable source rendering. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public Boolean getDisableSourceRendering() {
+        return disableSourceRendering;
+    }
+
+    /**
+     * Retrieves value of quick scan. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public Boolean getQuick() {
+        return quick;
+    }
+
+    /**
+     * Retrieves the rules. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public String getRules() {
+        return rules;
+    }
+
+    /**
+     * Retrieves the number of workers. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public String getWorkers() {
+        return workers;
+    }
+
+    /**
+     * Retrieves the RMI worker max heap. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public String getRmiWorkerMaxHeap() {
+        return rmiWorkerMaxHeap;
+    }
+
+    /**
+     * Retrieves the thread count. This is a per-build config item.
+     * This method must match the value in <tt>config.jelly</tt>.
+     */
+    public String getThreadCount() {
+        return threadCount;
+    }
+
+    /**
      * This method is called whenever the build step is executed.
      *
      * @param build    A Build object
@@ -109,7 +308,7 @@ public class FortifyCloudScanBuilder extends Builder implements Serializable {
     public boolean perform(final AbstractBuild build, final Launcher launcher, final BuildListener listener)
             throws InterruptedException, IOException {
 
-       final String args = generateArgs(build, listener);
+       final String[] args = generateArgs(build, listener);
 
        return launcher.getChannel().call(new MasterToSlaveCallable<Boolean, IOException>() {
            public Boolean call() throws IOException {
@@ -125,8 +324,70 @@ public class FortifyCloudScanBuilder extends Builder implements Serializable {
      * @param build an AbstractBuild object
      * @return fortifycloudscan Options
      */
-    private String generateArgs(AbstractBuild build, BuildListener listener) {
-        return null;
+    private String[] generateArgs(AbstractBuild build, BuildListener listener) {
+        List<String> command = new ArrayList<String>();
+
+        // Check if the path to the cloudscan executable was specified
+        String exePath = this.getDescriptor().getExePath();
+        if (StringUtils.isNotBlank(exePath)) {
+            command.add(exePath);
+        } else {
+            // Path was not specified, so just default to running 'cloudscan'. Must be in the path.
+            command.add("cloudscan");
+        }
+        if (useSsc) {
+            append(command, this.getDescriptor().getSscUrl(), "-sscurl");
+            append(command, sscToken, "-ssctoken");
+            append(command, null, "start");
+            append(command, null, "-upload");
+            append(command, versionId, "-versionid");
+            append(command, upToken, "-uptoken");
+        } else {
+            append(command, this.getDescriptor().getControllerUrl(), "-url");
+            append(command, null, "start");
+        }
+        append(command, buildId, "-b");
+        append(command, null, "-scan");
+        // Everthing appearing after -scan are parameters specific to sourceanalyzer
+        append(command, xmx, "-Xmx");
+        append(command, buildLabel, "-build-label");
+        append(command, buildProject, "-build-project");
+        append(command, buildVersion, "-build-version");
+        append(command, scanArgs, scanArgs);
+        append(command, disableDefaultRuleType, "-disable-default-rule-type");
+        append(command, encoding, "-encoding");
+        append(command, filter, "-filter");
+        append(command, noDefaultIssueRules, "-no-default-issue-rules");
+        append(command, noDefaultRules, "-no-default-rules");
+        append(command, noDefaultSourceRules, "-no-default-source-rules");
+        append(command, noDefaultSinkRules, "-no-default-sink-rules");
+        append(command, disableSourceRendering, "-disable-source-rendering");
+        append(command, quick, "-quick");
+        append(command, rules, "-rules");
+        append(command, workers, "-j");
+        append(command, rmiWorkerMaxHeap, "-Dcom.fortify.sca.RmiWorkerMaxHeap");
+        append(command, threadCount, "-Dcom.fortify.sca.ThreadCount");
+
+        Object[] objectList = command.toArray();
+        return Arrays.copyOf(objectList, objectList.length, String[].class);
+    }
+
+    private void append(List<String> command, Object confItem, String arg) {
+        if (confItem == null && arg != null) {
+            command.add(arg);
+        }
+        if (confItem instanceof String) {
+            String value = (String)confItem;
+            if (StringUtils.isNotBlank(value)) {
+                command.add(arg);
+                command.add(value);
+            }
+        } else if (confItem instanceof Boolean) {
+            boolean value = (Boolean)confItem;
+            if (value) {
+                command.add(arg);
+            }
+        }
     }
 
     /**
