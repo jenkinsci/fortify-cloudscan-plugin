a8096a58bb61c85c7f7b443b1779f88eecd476c9
Steve Springett <steve@item-a10907.wks.axway.int>
2015-10-21 11:27:21 -0500

    Single imports

--- src/main/java/org/jenkinsci/plugins/fortifycloudscan/FortifyCloudScanExecutor.java
+++ src/main/java/org/jenkinsci/plugins/fortifycloudscan/FortifyCloudScanExecutor.java
@@ -17,7 +17,11 @@ package org.jenkinsci.plugins.fortifycloudscan;
 
 import hudson.model.BuildListener;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Serializable;
 
 /**
  * This class is called by the FortifyCloudScanBuilder (the Jenkins build-step plugin).
